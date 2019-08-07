d6b80dfdf7e428f41ebfea7ad82e28866a654d7c
Lázaro Vieira <ryuzaki_mrl@hotmail.com>
2019-08-07 07:57:52 -0300

    GB: divider is a single 16 bits register

--- timer.cpp
+++ timer.cpp
@@ -3,28 +3,22 @@
 #include "cpu.h"
 #include "mem.h"
 
-static unsigned int prev_time;
-static unsigned int elapsed;
 static unsigned int ticks;
-static unsigned int divticks;
-
 static unsigned char tac;
-static unsigned int started;
+static unsigned char started;
 static unsigned int speed = 1024;
 static unsigned int counter;
-static unsigned char divider;
+static uint16_t divider;
 static unsigned char modulo;
 
-void timer_set_div(unsigned char v)
+void timer_reset_div(void)
 {
-	(void) v;
-	divticks = divider = 0;
-	//divider = 0;
+	divider = 0;
 }
 
 unsigned char timer_get_div(void)
 {
-	return divider;
+	return (divider >> 8);
 }
 
 void timer_set_counter(unsigned char v)
@@ -62,43 +56,10 @@ unsigned char timer_get_tac(void)
 	return tac;
 }
 
-static void timer_tick(void)
-{
-	/* 1/262144Hz has elapsed */
-	++ticks;
-
-	/* Divider updates at 16384Hz */
-	if(!(ticks & 15))
-	{
-		++divider;
-		//ticks = 0;
-	}
-
-	if(!started)
-		return;
-
-	if(!(ticks & (speed-1)))
-	{
-		if(++counter >= 0x100)
-		{
-			interrupt(INTR_TIMER);
-			counter = modulo;
-		}
-		//ticks = 0;
-	}
-}
-
 void timer_cycle(unsigned int delta)
 {
-	//unsigned char* mem = mem_get_bytes();
-	delta = delta << 2;
-	
-	divticks += delta;
-	if(divticks >= 256) {
-		//divticks -= 256;
-		divticks = 0;
-		++divider;
-	}
+	delta *= 4;
+	divider += delta;
 	if (started) {
 		ticks += delta;
 		if(ticks >= speed) {
@@ -110,11 +71,4 @@ void timer_cycle(unsigned int delta)
 			}
 		}
 	}
-
-	// elapsed += delta * 4; /* 4 cycles to a timer tick */
-	// while(elapsed >= 16)
-	// {
-		// timer_tick();
-		// elapsed -= 16;	/* keep track of the time overflow */
-	// }
 }
